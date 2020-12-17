b247a7053364bb8d2338ab62cb5a4f450a7a347d
HJNW <73272694+HJNW@users.noreply.github.com>
2020-12-17 12:43:46 +0800

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -5,3 +5,12 @@ yum -y install epel-release
 yum -y install nginx
 
 systemctl start nginx 
+
+cat > /usr/share/nginx/html/index.html << EOF
+<center>
+<h1>Hello, CMP</h1>
+<h2>Let's rock</h2>
+</center>
+
+EOF
+
