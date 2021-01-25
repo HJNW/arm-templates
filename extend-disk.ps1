bffec4ab7a85e8e893cb953699746b5f5961cd35
HJNW <73272694+HJNW@users.noreply.github.com>
2021-01-25 14:51:09 +0800

    Create extend-disk.ps1

---
+++ extend-disk.ps1
@@ -0,0 +1,17 @@
+# Bek Volume ignored
+
+$volumes = Get-Volume | ? { $_.DriveLetter -gt 69 }
+
+foreach ( $volume in $volumes ) {
+	$driveLetter = $volume.DriveLetter
+	echo $driveLetter
+	
+	$maxSize = (Get-PartitionSupportedSize -DriveLetter $driveLetter).SizeMax
+	$size = (Get-Partition -DriveLetter $driveLetter).size
+	
+  # At least 1GB available
+	if ( $maxSize - $size -gt 1000000000 ) {
+		Resize-Partition -DriveLetter $driveLetter -Size $maxSize
+	}
+}
+
