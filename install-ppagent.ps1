89be2c52ab61ec68a43279ce571b634734af68f8
HJNW <73272694+HJNW@users.noreply.github.com>
2021-03-26 07:43:34 +0100

    Update install-ppagent.ps1

--- install-ppagent.ps1
+++ install-ppagent.ps1
@@ -1,6 +1,9 @@
+"10.0.0.7 puppetent.rf5ltfv2bwtuzb53jl1202hspb.fx.internal.cloudapp.net" | Out-File -Encoding UTF8 -Append C:\Windows\System32\drivers\etc\hosts
+
 [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; 
 [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; 
 
-$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://puppetent.rf5ltfv2bwtuzb53jl1202hspb.fx.internal.cloudapp.net:8140/packages/current/install.ps1', 'install.ps1'); 
+$webClient = New-Object System.Net.WebClient; 
+$webClient.DownloadFile('https://puppetent.rf5ltfv2bwtuzb53jl1202hspb.fx.internal.cloudapp.net:8140/packages/current/install.ps1', 'install.ps1'); 
 
 .\install.ps1 -v
